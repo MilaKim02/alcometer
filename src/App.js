0f7ecf54124156e5af0058af93c506e3ec91bdf6
MilaKim02 <n2kimi01@students.oamk.fi>
2023-01-27 18:33:29 +0200

    html-rungon luonti

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [weight, setWeight] = useState(90)
+  const [gender, setGender] = useState("male")
+  const [bottles, setBottles] = useState(1)
+  const [time, setTime] = useState(1)
+  const [result, setResult] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h3>Alcometer</h3>
+    <form>
+      <div>
+        <label>Weight:</label>
+        <input name="weight" type="number" step="1"></input>
+      </div>
+      <div>
+        <label>Gender:</label>
+        <input type="radio" name="gender" value="male" defaultChecked ></input><label>Male</label>
+        <input type="radio" name="gender" value="female"></input><label>Female</label>
+      </div>
+      <div>
+        <label>Number of beer bottles drunk:</label>
+        <select name="bottles">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+      <div>
+        <label>Time:</label>
+       <select name="time">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+       </select>
+      </div>
+      <div>
+      <output></output>
+      </div>
+      <button>Calculate</button>
+   </form>
+    
+    </>
+    
   );
 }
 
